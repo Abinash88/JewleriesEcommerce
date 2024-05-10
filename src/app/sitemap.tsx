d51f2275116e727018afc20d01d82cc908daeb5e
Abinash88 <subediabinas@gmail.com>
2024-05-10 14:04:27 +0545

    feat: adding the site url

--- src/app/sitemap.tsx
+++ src/app/sitemap.tsx
@@ -1,57 +1,68 @@
-import { fetchGetRequest } from "@/Components/lib/Helper";
-import { NavBarDataTypes } from "@/DataSource/Types";
 
 
 const sitemap = async () => {
     const changeFrequency = 'daily';
 
-    const fetchData: NavBarDataTypes[] = await fetchGetRequest({ endpoint: `api/menu-items/` })
-    const nav = fetchData?.map((item, index) => {
-        return {
-            url: `${process.env.NEXT_PUBLIC_SITEMAP_URL}${item?.slug}`,
+
+    const SiteMaps = [
+        {
+            url: `${process.env.NEXT_PUBLIC_SITEMAP_URL}`,
             lastModified: new Date().toISOString(),
             changeFrequency,
             priority: 0.8,
-        }
-    })
-
-
-    const department = fetchData?.map((item, index) => {
-        if (item?.slug === "departments") {
-            return item?.child_navs?.map((subitem, index) => {
-                return {
-                    url: `${process.env.NEXT_PUBLIC_SITEMAP_URL}departments/${subitem.slug}`,
-                    lastModified: new Date().toISOString(),
-                    changeFrequency,
-                    priority: 0.8,
-                }
-            })
-        }
-    }).filter((item, _) => item)[0];
-
-    const services = fetchData?.map((item, index) => {
-        if (item?.slug === "services") {
-            return item?.child_navs?.map((subitem, index) => {
-                return {
-                    url: `${process.env.NEXT_PUBLIC_SITEMAP_URL}services/${subitem.slug}`,
-                    lastModified: new Date().toISOString(),
-                    changeFrequency,
-                    priority: 0.8,
-                }
-            })
-        }
-        
-    }).filter((item, _) => item)[0];
-
-    if (department && services)
-
-        return [{
-            url: process.env.NEXT_PUBLIC_SITEMAP_URL,
+        },
+        {
+            url: `${process.env.NEXT_PUBLIC_SITEMAP_URL}profile/`,
             lastModified: new Date().toISOString(),
             changeFrequency,
-            priority: 1,
-        }, ...nav, ...department, ...services,
-        ]
+            priority: 0.8,
+        },
+        {
+            url: `${process.env.NEXT_PUBLIC_SITEMAP_URL}faqs/`,
+            lastModified: new Date().toISOString(),
+            changeFrequency,
+            priority: 0.8,
+        },
+        {
+            url: `${process.env.NEXT_PUBLIC_SITEMAP_URL}about-us/`,
+            lastModified: new Date().toISOString(),
+            changeFrequency,
+            priority: 0.8,
+        },
+        {
+            url: `${process.env.NEXT_PUBLIC_SITEMAP_URL}product/`,
+            lastModified: new Date().toISOString(),
+            changeFrequency,
+            priority: 0.8,
+        },
+        {
+            url: `${process.env.NEXT_PUBLIC_SITEMAP_URL}favourites/`,
+            lastModified: new Date().toISOString(),
+            changeFrequency,
+            priority: 0.8,
+        },
+        {
+            url: `${process.env.NEXT_PUBLIC_SITEMAP_URL}bags/`,
+            lastModified: new Date().toISOString(),
+            changeFrequency,
+            priority: 0.8,
+        },
+        {
+            url: `${process.env.NEXT_PUBLIC_SITEMAP_URL}my-order/`,
+            lastModified: new Date().toISOString(),
+            changeFrequency,
+            priority: 0.8,
+        },
+        {
+            url: `${process.env.NEXT_PUBLIC_SITEMAP_URL}find-undertone/`,
+            lastModified: new Date().toISOString(),
+            changeFrequency,
+            priority: 0.8,
+        },
+    ]
+
+
+    return [...SiteMaps]
 }
 
 export default sitemap;
